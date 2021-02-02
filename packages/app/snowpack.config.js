a0512df0f53d347278ad4c8d4eb081e8386fa8c6
Edward Mulraney <edmulraney@gmail.com>
2021-02-02 16:29:54 +0000

    use typescript path references

--- packages/app/snowpack.config.js
+++ packages/app/snowpack.config.js
@@ -5,25 +5,20 @@ module.exports = {
     src: '/',
     '../launch-kit': '/@edmulraney/launch-kit',
     '../components': '/@edmulraney/components',
-   },
+  },
   plugins: [
     '@snowpack/plugin-typescript',
     '@snowpack/plugin-react-refresh',
   ],
   routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
   optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
   },
   packageOptions: {
   },
   devOptions: {
   },
   buildOptions: {
-    // sourceMap: true,
   },
   alias: {
     '@edmulraney/launch-kit': '../launch-kit',
---
+++ packages/launch-kit/src/index.tsx
@@ -0,0 +1,1 @@
+export { Setup } from './setup/setup'
