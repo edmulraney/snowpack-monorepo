5b3bf7042cc16b807f35f9c59c5c6c16cc33e551
Edward Mulraney <edmulraney@gmail.com>
2021-02-04 10:53:27 +0000

    add storybook

--- packages/components/snowpack.config.js
+++ packages/components/snowpack.config.js
@@ -1,22 +1,10 @@
 /** @type {import('snowpack").SnowpackUserConfig } */
 module.exports = {
-  mount: {
-  },
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-typescript',
   ],
-  routes: [
-  ],
-  optimize: {
-  },
   packageOptions: {
     external: ['react'],
   },
-  devOptions: {
-  },
-  buildOptions: {
-  },
-  alias: {
-  }
 };
